b88855d2194905ae910743e33718f7cc6fcb2d48
Gho$t/ <mynamem429@gmail.com>
2025-07-09 17:02:08 +0300

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -209,14 +209,13 @@ panel_install(){
 
     curl -Lo panel.tar.gz https://github.com/ghost-dev-gr/panel/releases/latest/download/panel.tar.gz
     tar -xzvf panel.tar.gz
-    echo "removing panel folder if found"
     rm panel.tar.gz
     if [ -d "panel" ]; then
         mv panel/* ./
         mv panel/.* ./
         rmdir panel
     fi
-
+    
     mkdir -p storage bootstrap/cache vendor /var/www/.cache/composer/vcs
     chown -R www-data:www-data /var/www/pterodactyl /var/www/.cache
     chmod -R 755 /var/www/pterodactyl/bootstrap/cache
